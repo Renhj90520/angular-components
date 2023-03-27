dd8659adc96b0142ea22d14ab67a6edffaf3a90b
Ren Haojie <renhaojie520@live.cn>
2023-03-27 08:23:21 +0800

    scroll-navbar HostListener('window:scroll')

--- src/app/scroll-navbar/scroll-navbar.component.ts
+++ src/app/scroll-navbar/scroll-navbar.component.ts
@@ -1,39 +1,49 @@
-import { Component, OnInit, ElementRef, ViewChild, Input, ViewRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Input,
+  ViewRef,
+  HostListener,
+} from "@angular/core";
 @Component({
-  selector: 'app-scroll-navbar',
-  templateUrl: './scroll-navbar.component.html',
-  styleUrls: ['./scroll-navbar.component.css']
+  selector: "app-scroll-navbar",
+  templateUrl: "./scroll-navbar.component.html",
+  styleUrls: ["./scroll-navbar.component.css"],
 })
 export class ScrollNavbarComponent implements OnInit {
   defaultOffset: number;
-  @ViewChild('logo') logo;
-  @ViewChild('items') itemswraper;
+  @ViewChild("logo") logo;
+  @ViewChild("items") itemswraper;
 
   @Input() sectionsTop = [];
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
   ngOnInit() {
-    const items = this.itemswraper.nativeElement.children;
     this.defaultOffset = this.el.nativeElement.offsetTop;
-    window.onscroll = (event) => {
-      if (window.scrollY > this.defaultOffset) {
-        this.el.nativeElement.className = 'sticky';
-        this.logo.nativeElement.className = 'logo sticky';
-        this.itemswraper.nativeElement.className = 'shrink';
-      } else {
-        this.el.nativeElement.className = '';
-        this.logo.nativeElement.className = 'logo';
-        this.itemswraper.nativeElement.className = '';
+  }
+
+  @HostListener("window:scroll")
+  onScroll() {
+    const items = this.itemswraper.nativeElement.children;
+    if (window.scrollY > this.defaultOffset) {
+      this.el.nativeElement.className = "sticky";
+      this.logo.nativeElement.className = "logo sticky";
+      this.itemswraper.nativeElement.className = "shrink";
+    } else {
+      this.el.nativeElement.className = "";
+      this.logo.nativeElement.className = "logo";
+      this.itemswraper.nativeElement.className = "";
+    }
+    let largest = 0;
+    for (let i = 0; i < items.length; i++) {
+      if (window.scrollY >= this.sectionsTop[i]) {
+        largest = i;
       }
-      let largest = 0;
-      for (let i = 0; i < items.length; i++) {
-        if (window.scrollY >= this.sectionsTop[i]) {
-          largest = i;
-        }
-        items[i].className = '';
-      }
-      items[largest].className = 'current';
-    };
+      items[i].className = "";
+    }
+    items[largest].className = "current";
   }
 }
